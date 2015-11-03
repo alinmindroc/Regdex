9b2e462a98af2fbb7c7a181202d7aceb10de7639
Alin Mindroc <alin@debian>
2015-11-03 19:59:35 +0200

    add css and change structure

---
+++ public/javascripts/WordsController.js
@@ -0,0 +1,37 @@
+angular.module('app', ['ngRoute', 'ngResource'])
+.controller('WordsController', ['$scope', '$resource', function($scope, $resource){
+	$scope.wordLength = 5;
+	$scope.maxWordLength = 16;
+	$scope.knownLetters = new Array($scope.maxWordLength);
+	$scope.possibleLetters = "";
+
+	$scope.range = function(len){
+		return new Array(len);
+	}
+
+	$scope.go = function(){
+		params = {};
+
+		for(var i in $scope.knownLetters){
+			if($scope.knownLetters[i].length > 0)
+				params['l' + i] = $scope.knownLetters[i];
+		}
+
+		params['length'] = $scope.wordLength;
+		params['possibleLetters'] = $scope.possibleLetters;
+
+		var resource = $resource('/words', {});
+		var wordsResult = resource.query(params, function(){
+			$scope.result = wordsResult;
+		});
+	}
+
+
+}])
+.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: '/templates/words.html',
+		controller: 'WordsController'
+	});
+}]);
